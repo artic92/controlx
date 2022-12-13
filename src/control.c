942aa59cc6caef6ab23ba0ac096b35c3d6c384af
Antonio Riccio <antonio.riccio.27@gmail.com>
2022-12-13 16:40:30 +0100

    control.c: improve code readability

--- src/control.c
+++ src/control.c
@@ -30,41 +30,48 @@ void control(channel_t* cmd_ch, channel_t* data_ch_rx, channel_t* data_ch_tx)
 
    mex_tx.mtype = ID_CTR;
 
-   fprintf(stdout, "[%i] control: waiting for messages...\n", getpid());
-
    while (true)
    {
+      fprintf(stdout, "[%i] control: waiting for messages...\n", getpid());
+
       channel_retrieve_nonblock(cmd_ch, &mex_rx);
-      if(mex_rx.mtype == TERMINATE)
+      if((mex_rx.mtype == TERMINATE) && (mex_rx.mvalue == TERMINATE))
       {
-         fprintf(stdout, "[%i] control: received termination command, SHUTTING DOWN...\n", getpid());
+         fprintf(stdout, "[%i] control: received termination command, SHUTTING DOWN...\n",
+            getpid());
          sleep(5);
          exit(EXIT_SUCCESS);
       }
 
       channel_retrieve_block(data_ch_rx, &mex_rx);
-      fprintf(stdout, "[%i] control: received data: type %li, value %i \n", getpid(), mex_rx.mtype, mex_rx.mvalue);
+      fprintf(stdout, "[%i] control: received data: type %li, value %i \n",
+         getpid(), mex_rx.mtype, mex_rx.mvalue);
 
       control_law(&mex_rx.mvalue, &mex_tx.mvalue);
 
       channel_push_nonblock(data_ch_tx, &mex_tx);
-      fprintf(stdout, "[%i] control: sent data to actuators: type %li, value %i\n", getpid(), mex_tx.mtype, mex_tx.mvalue);
+      fprintf(stdout, "[%i] control: transmit data: type %li, value %i\n",
+         getpid(), mex_tx.mtype, mex_tx.mvalue);
 
       channel_retrieve_block(data_ch_rx, &mex_rx);
-      fprintf(stdout, "[%i] control: received data: type %li, value %i \n", getpid(), mex_rx.mtype, mex_rx.mvalue);
+      fprintf(stdout, "[%i] control: received data: type %li, value %i \n",
+         getpid(), mex_rx.mtype, mex_rx.mvalue);
 
       control_law(&mex_rx.mvalue, &mex_tx.mvalue);
 
       channel_push_nonblock(data_ch_tx, &mex_tx);
-      fprintf(stdout, "[%i] control: sent data to actuators: type %li, value %i\n", getpid(), mex_tx.mtype, mex_tx.mvalue);
+      fprintf(stdout, "[%i] control: transmit data: type %li, value %i\n",
+         getpid(), mex_tx.mtype, mex_tx.mvalue);
 
       channel_retrieve_block(data_ch_rx, &mex_rx);
-      fprintf(stdout, "[%i] control: received data: type %li, value %i \n", getpid(), mex_rx.mtype, mex_rx.mvalue);
+      fprintf(stdout, "[%i] control: received data: type %li, value %i \n",
+         getpid(), mex_rx.mtype, mex_rx.mvalue);
 
       control_law(&mex_rx.mvalue, &mex_tx.mvalue);
 
       channel_push_nonblock(data_ch_tx, &mex_tx);
-      fprintf(stdout, "[%i] control: sent data to actuators: type %li, value %i\n", getpid(), mex_tx.mtype, mex_tx.mvalue);
+      fprintf(stdout, "[%i] control: transmit data: type %li, value %i\n",
+         getpid(),mex_tx.mtype, mex_tx.mvalue);
    }
 }
 
@@ -80,46 +87,53 @@ void vote(channel_t* cmd_ch, channel_t* data_ch_rx, channel_t* data_ch_tx, int i
    channel_create(data_ch_tx, CH1);
    channel_create(cmd_ch, CHCMD);
 
-   fprintf(stdout, "[%i] voter: waiting for messages...\n", getpid());
-
    while (true)
    {
+      fprintf(stdout, "[%i] voter: waiting for messages...\n", getpid());
+
       channel_retrieve_nonblock(cmd_ch, &mex_rx1);
-      if(mex_rx1.mtype == TERMINATE)
+      if((mex_rx1.mtype == TERMINATE) && (mex_rx1.mvalue == TERMINATE))
       {
          fprintf(stdout, "[%i] voter: received termination command, SHUTTING DOWN...\n", getpid());
          exit(EXIT_SUCCESS);
       }
 
       channel_retrieve_block(data_ch_rx, &mex_rx1);
-      fprintf(stdout, "[%i] voter: received data: type %li, value %i \n", getpid(), mex_rx1.mtype, mex_rx1.mvalue);
+      fprintf(stdout, "[%i] voter: received data: type %li, value %i \n",
+         getpid(), mex_rx1.mtype, mex_rx1.mvalue);
 
       channel_retrieve_block(data_ch_rx, &mex_rx2);
-      fprintf(stdout, "[%i] voter: received data: type %li, value %i \n", getpid(), mex_rx2.mtype, mex_rx2.mvalue);
+      fprintf(stdout, "[%i] voter: received data: type %li, value %i \n",
+         getpid(), mex_rx2.mtype, mex_rx2.mvalue);
 
       if(mex_rx1.mvalue != mex_rx2.mvalue)
       {
          channel_retrieve_block(data_ch_rx, &mex_rx3);
-         fprintf(stdout, "[%i] voter: received data: type %li, value %i \n", getpid(), mex_rx3.mtype, mex_rx3.mvalue);
+         fprintf(stdout, "[%i] voter: received data: type %li, value %i \n",
+            getpid(), mex_rx3.mtype, mex_rx3.mvalue);
 
          if(mex_rx2.mvalue == mex_rx3.mvalue)
          {
-            fprintf(stdout, "[%i] voter: 2-out-3 consensus reached, values 1:%i, 2:%i, 3:%i\n", getpid(), mex_rx1.mvalue, mex_rx2.mvalue, mex_rx3.mvalue);
+            fprintf(stdout, "[%i] voter: 2-out-3 consensus reached, values 1:%i, 2:%i, 3:%i\n",
+               getpid(), mex_rx1.mvalue, mex_rx2.mvalue, mex_rx3.mvalue);
             mex_tx.mvalue = mex_rx3.mvalue;
          }
          else
          {
-            fprintf(stdout, "[%i] voter: NO consensus reached, sending 0. values 1:%i, 2:%i, 3:%i\n", getpid(), mex_rx1.mvalue, mex_rx2.mvalue, mex_rx3.mvalue);
+            fprintf(stdout, "[%i] voter: NO consensus reached, sending 0. values 1:%i, 2:%i, 3:%i\n",
+               getpid(), mex_rx1.mvalue, mex_rx2.mvalue, mex_rx3.mvalue);
             mex_tx.mtype = 0;
          }
       }
       else
       {
-         fprintf(stdout, "[%i] voter: 3-out-3 consensus reached, values 1:%i, 2:%i\n", getpid(), mex_rx1.mvalue, mex_rx2.mvalue);
+         fprintf(stdout, "[%i] voter: 3-out-3 consensus reached, values 1:%i, 2:%i\n",
+            getpid(), mex_rx1.mvalue, mex_rx2.mvalue);
          mex_tx.mvalue = mex_rx1.mvalue;
       }
 
       channel_push_nonblock(data_ch_tx, &mex_tx);
-      fprintf(stdout, "[%i] voter: sent data to control: type %li, value %i\n", getpid(), mex_tx.mtype, mex_tx.mvalue);
+      fprintf(stdout, "[%i] voter: sent data to control: type %li, value %i\n",
+         getpid(), mex_tx.mtype, mex_tx.mvalue);
    }
 }
